137c8c9946cbe1c4af2240caeb8b13861cc082d1
Dejan Zivkovic <dejan.zivkovic90@gmail.com>
2018-04-02 09:28:00 +0200

    Added mirror engine to handle multiple mirrors reflecting each other

--- src/scripts/modules/index.js
+++ src/scripts/modules/index.js
@@ -2,8 +2,9 @@ import * as THREE from 'three';
 import 'three/examples/js/controls/OrbitControls'
 
 import MirrorRenderer from './mirror';
+import MirrorEngine from './mirror-engine';
 
-const FRAME_PADDING = 0.1;
+
 
 export default class MirrorsRendering {
 
@@ -24,20 +25,18 @@ export default class MirrorsRendering {
 
 		this._controls = new THREE.OrbitControls(this._camera, this._renderer.domElement);
 
-		this._mirrorRenderer = new MirrorRenderer(this._camera, new THREE.Vector2(5.0, 5.0));
-		this._mirrorRenderer.mesh.position.set(0.0, 5.0, -10.0);
-		this._scene.add(this._mirrorRenderer.mesh);
-		
-		this._mirrorFrame = this.makeMirrorFrame(this._mirrorRenderer, 0x618930);
-		this._scene.add(this._mirrorFrame);
-	
-		this._mirrorRenderer2 = new MirrorRenderer(this._camera, new THREE.Vector2(20.0, 20.0));
-		this._mirrorRenderer2.mesh.lookAt(1.0, 0.0, 0.0);
-		this._mirrorRenderer2.mesh.position.set(-10.0, 10.1, 0.0);
-		this._scene.add(this._mirrorRenderer2.mesh);
-		
-		this._mirrorFrame2 = this.makeMirrorFrame(this._mirrorRenderer2, 0x618930);
-		this._scene.add(this._mirrorFrame2);
+		this._mirrorEngine = new MirrorEngine(this._scene, this._camera);
+
+		this._mirrorEngine.addMirrorWithFrame(new THREE.Vector2(10, 10),
+											0x618930,
+											new THREE.Vector3(-10.0, 5.1, 0.0),
+											new THREE.Vector3(1.0, 0.0, 0.0));
+
+		this._mirrorEngine.addMirrorWithFrame(new THREE.Vector2(12, 12),
+											  0x618930,
+											  new THREE.Vector3(0.0, 6.1, -10.0),
+											  new THREE.Vector3(0.0, 0.0, 1.0));
+
 	
 
 		let groundPlaneGeometry = new THREE.PlaneGeometry(20, 20, 20, 20);
@@ -62,54 +61,21 @@ export default class MirrorsRendering {
 		this.start();
 	}
 
-	makeMirrorFrame(mirrorRenderer, color){
-		let mirrorSize = mirrorRenderer.mirrorSize.clone().add(new THREE.Vector2(FRAME_PADDING, FRAME_PADDING));
-		let frameGeometry = new THREE.PlaneGeometry(mirrorSize.x, mirrorSize.y, Math.round(Math.max(mirrorSize.x, mirrorSize.y)));
-		let frameMaterial = new THREE.MeshPhongMaterial({color: color, side: THREE.DoubleSide});
-		let frameMesh = new THREE.Mesh(frameGeometry, frameMaterial);
-		frameMesh.position.copy(mirrorRenderer.mesh.position);
-		frameMesh.quaternion.copy(mirrorRenderer.mesh.quaternion);
-		frameMesh.updateMatrix();
-		let mirrorDirection = mirrorRenderer.mesh.getWorldDirection();
-		mirrorDirection.normalize();
-		mirrorDirection.multiplyScalar(0.01);
-		frameMesh.position.sub(mirrorDirection);
-		return frameMesh;
-
-	}
+	
 
 	start() {
 		this._cube.rotation.y += 0.01;
 		this._cube.rotation.x += 0.01;
-
+		
 		this.render();
+		this._mirrorEngine.update();
 		this._controls.update();
-		this._mirrorRenderer.update();
-		this._mirrorRenderer2.update();
+		
 		requestAnimationFrame(this.start.bind(this));		
 	}
 
 	render() {
-
-		this._mirrorRenderer.setExcludedObjects([
-			this._mirrorRenderer2.mesh,
-			this._mirrorFrame,
-			this._mirrorFrame2
-		]);
-		this._mirrorRenderer.render(this._renderer, this._scene);
-
-		this._mirrorRenderer2.setExcludedObjects([
-			this._mirrorFrame2
-		]);
-
-		this._mirrorRenderer2.render(this._renderer, this._scene);
-
-		this._mirrorRenderer.setExcludedObjects([
-			this._mirrorFrame
-		]);
-
-		this._mirrorRenderer.render(this._renderer, this._scene);
-
+		this._mirrorEngine.render(this._renderer);
 		this._renderer.render(this._scene, this._camera);
 	}
 }
---
+++ src/scripts/modules/mirror-engine.js
@@ -0,0 +1,78 @@
+import MirrorRenderer from './mirror';
+
+const FRAME_PADDING = 0.1;
+
+export default class MirrorEngine {
+
+    constructor(scene, camera){
+        this._scene = scene;
+        this._camera = camera;
+
+        this._mirrorsWithFrames = [];
+    }
+
+    makeMirrorFrame(mirrorRenderer, color){
+		let mirrorSize = mirrorRenderer.mirrorSize.clone().add(new THREE.Vector2(FRAME_PADDING, FRAME_PADDING));
+		let frameGeometry = new THREE.PlaneGeometry(mirrorSize.x, mirrorSize.y, Math.round(Math.max(mirrorSize.x, mirrorSize.y)));
+		let frameMaterial = new THREE.MeshPhongMaterial({color: color, side: THREE.DoubleSide});
+		let frameMesh = new THREE.Mesh(frameGeometry, frameMaterial);
+		frameMesh.position.copy(mirrorRenderer.mesh.position);
+		frameMesh.quaternion.copy(mirrorRenderer.mesh.quaternion);
+		frameMesh.updateMatrix();
+		let mirrorDirection = mirrorRenderer.mesh.getWorldDirection();
+		mirrorDirection.normalize();
+		mirrorDirection.multiplyScalar(0.01);
+		frameMesh.position.sub(mirrorDirection);
+		return frameMesh;
+
+	}
+
+    addMirrorWithFrame(mirrorSize, frameColorHex, position, lookAt){
+
+        let mirrorRenderer = new MirrorRenderer(this._camera, mirrorSize);
+        mirrorRenderer.mesh.lookAt(lookAt);
+        mirrorRenderer.mesh.position.copy(position);
+       
+		this._scene.add(mirrorRenderer.mesh);
+		
+		let mirrorFrame = this.makeMirrorFrame(mirrorRenderer, frameColorHex);
+        this._scene.add(mirrorFrame);
+
+        this._mirrorsWithFrames.push({
+            mirrorRenderer: mirrorRenderer,
+            frame: mirrorFrame
+        });
+    }
+
+    update() {
+        this._mirrorsWithFrames.forEach((mirrorWithFrame) => {
+            mirrorWithFrame.mirrorRenderer.update();
+        });
+    }
+
+    render(renderer) {
+
+        this._mirrorsWithFrames.forEach((mirrorWithFrame, index) => {
+
+            let excludedObjects = [];
+
+            this._mirrorsWithFrames.forEach((m, i) => {
+                if (i > index){
+                    excludedObjects.push(m.mirrorRenderer.mesh);
+                    excludedObjects.push(m.frame);
+                }
+            });
+
+            excludedObjects.push(mirrorWithFrame.frame);
+            mirrorWithFrame.mirrorRenderer.setExcludedObjects(excludedObjects);
+            mirrorWithFrame.mirrorRenderer.render(renderer, this._scene);
+        });
+
+        this._mirrorsWithFrames.forEach((mirrorWithFrame, index) => {
+            mirrorWithFrame.mirrorRenderer.setExcludedObjects([mirrorWithFrame.frame]);
+            mirrorWithFrame.mirrorRenderer.render(renderer, this._scene);
+        });
+    }
+
+
+}
